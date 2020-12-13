b3c685a4d5296ed9eef1ca16cc39f9a566fff8ee
madelinemc <madeline.coons@gmail.com>
2020-12-13 17:55:38 -0600

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+    
+    def new
+    end
+
+    def create
+        @user = User.find_by(name: params[:user][:name])
+        if @user
+            return head(:forbidden) unless @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+        else
+            redirect_to login_path
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        redirect_to '/'
+    end
+
+end
